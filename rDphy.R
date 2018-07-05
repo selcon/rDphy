350d4ed27851cd44b8f2af97de93ad4ee3eff708
kottn <35750723+kottn@users.noreply.github.com>
2018-07-05 16:31:56 +0900

    add files

---
+++ rDphy.R
@@ -0,0 +1,29 @@
+require(ggplot2)
+require(hexSticker)
+
+source("https://bioconductor.org/biocLite.R")
+biocLite("ChIPseeker")
+require(ChIPseeker)
+data("tagMatrixList")
+
+p <- plotAvgProf(tagMatrixList[-c(4,5)], xlim=c(-3000, 3000),
+                 conf=0.99,resample=1000)
+d = p$data
+head(d)
+
+require(ggplot2)
+
+g <- ggplot(d, aes(pos, value, color=.id)) +
+  geom_ribbon(aes(ymin = Lower, ymax = Upper, fill=.id),
+              linetype = 0, alpha = 0.25) +
+  geom_line(size=.2) +
+  scale_color_brewer(palette="Set1") +
+  scale_fill_brewer(palette="Set1") +
+  theme_void() +
+  ggimage::theme_transparent(legend.position='none')
+
+g
+sticker(g, package="　",
+        p_size=6, s_x=.98, s_y=.88, s_width=1.4, s_height=1.1,
+        h_fill = "#114466", h_color="#001030")
+
